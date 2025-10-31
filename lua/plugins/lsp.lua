3ed25932321c3063b3b426b5dc0525a411c7a453
Theo Brigitte <theo.brigitte@gmail.com>
2025-10-31 12:08:03 +0100

    vim: add lsp configuration

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,11 +1,18 @@
 return {
 	{
 		"mason-org/mason.nvim",
-		opts = {}
 	},
 	{
 		"mason-org/mason-lspconfig.nvim",
-		opts = {},
+		opts = {
+			ensure_installed = {
+				"copilot-language-server",
+				"gofumpt",
+				"goimports-reviser",
+				"gopls",
+				"shellcheck",
+			},
+		},
 		dependencies = {
 			{ "mason-org/mason.nvim", opts = {} },
 			"neovim/nvim-lspconfig",
