51baab8cb2326d5b83e9e58d8dab527fe14d00eb
Theo Brigitte <theo.brigitte@gmail.com>
2025-11-01 05:29:08 +0100

    vim: add lsp tools to configuration

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,24 +1,44 @@
 return {
+	-- Mason: package manager for LSP servers, formatters, linters, etc.
 	{
 		"mason-org/mason.nvim",
-	},
-	{
-		"mason-org/mason-lspconfig.nvim",
 		opts = {
-			ensure_installed = {
-				"copilot-language-server",
-				"gofumpt",
-				"goimports-reviser",
-				"gopls",
-				"shellcheck",
+			ui = {
+				border = "rounded",
 			},
 		},
+	},
+
+	-- Mason LSP Config: bridges mason.nvim with nvim-lspconfig
+	{
+		"mason-org/mason-lspconfig.nvim",
 		dependencies = {
-			{ "mason-org/mason.nvim", opts = {} },
+			"mason-org/mason.nvim",
 			"neovim/nvim-lspconfig",
 		},
+		opts = {
+			-- Only LSP servers here
+			ensure_installed = {
+				"gopls",
+			},
+		},
 	},
+
+	-- Mason Tool Installer: auto-install formatters, linters, and other tools
 	{
-		"neovim/nvim-lspconfig",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependencies = {
+			"mason-org/mason.nvim",
+		},
+		opts = {
+			-- Non-LSP tools: formatters, linters, DAP servers, etc.
+			ensure_installed = {
+				"gofumpt",           -- Go formatter (stricter than gofmt)
+				"goimports-reviser", -- Go imports formatter
+				"shellcheck",        -- Shell script linter
+			},
+			auto_update = false,
+			run_on_start = true,
+		},
 	},
 }
