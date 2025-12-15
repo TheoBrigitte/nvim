04d9240bf6a58deabca7ac8534d0498af13cb664
Theo Brigitte <theo.brigitte@gmail.com>
2025-12-15 15:07:17 +0100

    vim: disable tmux navigation when zoomed

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -1,18 +1,24 @@
 return {
-	"christoomey/vim-tmux-navigator",
-	cmd = {
-		"TmuxNavigateLeft",
-		"TmuxNavigateDown",
-		"TmuxNavigateUp",
-		"TmuxNavigateRight",
-		"TmuxNavigatePrevious",
-		"TmuxNavigatorProcessList",
-	},
-	keys = {
-		{ "`<Left>", "<cmd>TmuxNavigateLeft<cr>" },
-		{ "`<Down>", "<cmd>TmuxNavigateDown<cr>" },
-		{ "`<Up>", "<cmd>TmuxNavigateUp<cr>" },
-		{ "`<Right>", "<cmd>TmuxNavigateRight<cr>" },
-		{ "`<Tab>", "<cmd>TmuxNavigatePrevious<cr>" },
-	},
+	{
+		"christoomey/vim-tmux-navigator",
+		cmd = {
+			"TmuxNavigateLeft",
+			"TmuxNavigateDown",
+			"TmuxNavigateUp",
+			"TmuxNavigateRight",
+			"TmuxNavigatePrevious",
+			"TmuxNavigatorProcessList",
+		},
+		keys = {
+			{ "`<Left>", "<cmd>TmuxNavigateLeft<cr>" },
+			{ "`<Down>", "<cmd>TmuxNavigateDown<cr>" },
+			{ "`<Up>", "<cmd>TmuxNavigateUp<cr>" },
+			{ "`<Right>", "<cmd>TmuxNavigateRight<cr>" },
+			{ "`<Tab>", "<cmd>TmuxNavigatePrevious<cr>" },
+		},
+		config = function()
+			-- Disable tmux navigator when zooming the Vim pane
+			vim.g.tmux_navigator_disable_when_zoomed = 1
+		end,
+	}
 }
