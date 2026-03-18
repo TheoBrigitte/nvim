14d6f12cff045a783cc0d150af9b693c6fc25221
Theo Brigitte <theo.brigitte@gmail.com>
2026-03-18 20:39:08 +0100

    nvim: lazy load treesitter

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,6 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
+  lazy = false,
   event = { "BufReadPre", "BufNewFile" },
   dependencies = {
     "nvim-treesitter/nvim-treesitter-textobjects",
